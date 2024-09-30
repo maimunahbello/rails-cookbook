0c696397f7e84f5b492a4fd58abbe2a46f4ad4f8
Maimunah Adeniji-Bello <maimunahbello05@gmail.com>
2024-09-30 11:02:37 +0100

    validated

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :recipe
   belongs_to :category
+
+  validates :comment, presence: true, length: { minimum: 6, too_short: 'must have at least 6 characters' }
+  validates :recipe_id, uniqueness: { scope: :category_id}
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,7 @@
 class Recipe < ApplicationRecord
   has_many :bookmarks
-  has_many :categories, through: :bookmarks
+
+  validates :description, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
 end
