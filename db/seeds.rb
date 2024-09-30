d9c15d25ab39ee263e6289b3923fa0dadfe1d068
Maimunah Adeniji-Bello <maimunahbello05@gmail.com>
2024-09-30 11:27:28 +0100

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,31 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Recipe.create!(
+  name: 'Spaghetti Carbonara',
+  description: "A true Italian Carbonara recipe, it's ready in about 30 minutes. There is no cream, only eggs, cheese, pancetta, and pepper.",
+  image_url: 'https://tinyurl.com/3eapsa38',
+  rating: 8.0
+)
+
+Recipe.create!(
+  name: 'Chicken Tikka Masala',
+  description: 'A popular Indian dish with marinated chicken pieces cooked in a rich, creamy tomato sauce with a variety of spices.',
+  image_url: 'https://tinyurl.com/2wec92mp',
+  rating: 9.0
+)
+
+Recipe.create!(
+  name: 'Margherita Pizza',
+  description: 'A classic Italian pizza made with a simple tomato sauce, fresh mozzarella, basil, and olive oil.',
+  image_url: 'https://tinyurl.com/2pt8uese',
+  rating: 7.0
+)
+
+Recipe.create!(
+  name: 'Beef Stroganoff',
+  description: 'A Russian dish consisting of sautéed pieces of beef served in a sauce with sour cream.',
+  image_url: 'https://tinyurl.com/y7cfmh4r',
+  rating: 8.0
+)
